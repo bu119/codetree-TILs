cc8f1acf71665c5a26b42d1ffc199482d1163b7b
bu119 <109335452+bu119@users.noreply.github.com>
2024-02-17 23:30:19 +0900

    Title: 팩맨, Time: 177ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240217/팩맨/pacman.py
+++ 240217/팩맨/pacman.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from collections import deque
 
 # 몬스터 이동 함수
 def moveMonster(x, y, k):
@@ -10,14 +11,14 @@ def moveMonster(x, y, k):
         nx = x + di[nk]
         ny = y + dj[nk]
         # 격자에 위치하고 몬스터 시체와 팩맨이 없으면 이동할 수 있다.
-        if 0 <= nx < 4 and 0 <= ny < 4 and dieMonsters[nx][ny] == 0 and (pi, pj) != (nx, ny):
-            return nx, ny, nk
+        if 0 <= nx < 4 and 0 <= ny < 4 and not dieMonsters.get((nx, ny)) and (pi, pj) != (nx, ny):
+            return (nx, ny), nk
         else:
             # 몬스터 시체가 있거나, 팩맨이 있는 경우거나 격자를 벗어나는 방향일 경우
             nk = (nk + 1) % 8
             cnt += 1
     # 움직일 수 없으면 유지
-    return x, y, k
+    return (x, y), k
 
 # 팩맨 이동 함수
 def movePacman(px, py):
@@ -26,15 +27,16 @@ def movePacman(px, py):
     # 이동 위치 저장
     route = set()
     # 몬스터 먹은 개수 저장
-    maxV = 0
-    
+    maxV = -1
+    # 상-좌-하-우의 우선순위
+    deq = deque()
     # 현재 위치, 먹은 개수, 이동 경로 저장
-    stack = [(px, py, 0, set())]
+    deq.append((px, py, 0, set()))
     # 이동 위치로 방문 체크
     visited = set()
 
-    while stack:
-        x, y, eatM, currRoute = stack.pop()
+    while deq:
+        x, y, eatM, currRoute = deq.popleft()
 
         # 방문 체크
         now = tuple(sorted(currRoute))
@@ -58,7 +60,12 @@ def movePacman(px, py):
                 # 새로운 경로 저장
                 nRoute = deepcopy(currRoute)
                 nRoute.add((nx, ny))
-                stack.append((nx, ny, eatM + liveMonster[nx][ny], nRoute))
+                # 몬스터 개수
+                mCnt = 0
+                if (nx, ny) in newMonsters:
+                    mCnt = len(newMonsters[(nx, ny)])
+
+                deq.append((nx, ny, eatM + mCnt, nRoute))
 
     return pnx, pny, route
 
@@ -69,19 +76,19 @@ m, t = map(int, input().split())
 pi, pj = map(int, input().split())
 pi -= 1
 pj -= 1
-# 살아있는 몬스터 수 저장
-liveMonster = [[0]*4 for _ in range(4)]
-# 몬스터 시체 수 저장
-dieMonsters = [[0]*4 for _ in range(4)]
-# 살아있는 몬스터 저장 (i, j, d)
-monsters = []
+# 살아있는 몬스터 저장 {(i, j): [d], ...}
+monsters = dict()
+# 몬스터 시체 수 저장 {위치: [방향, ...], ...}
+dieMonsters = dict()
 # 몬스터 위치, 방향 추가
 for _ in range(m):
     r, c, d = map(int, input().split())
-    r -= 1
-    c -= 1
-    monsters.append((r, c, d-1))
-    liveMonster[r][c] += 1
+    key = (r-1, c-1)
+    d -= 1
+    if key in monsters:
+        monsters[key].append(d)
+    else:
+        monsters[key] =[d]
 
 # 상 부터 반시계 방향
 di = [-1, -1, 0, 1, 1, 1, 0, -1] 
@@ -91,45 +98,51 @@ for _ in range(t):
     # 1.몬스터 복제
     copyMonsters = deepcopy(monsters)
 
-    # 몬스터 이동 위치 저장
-    newMonsters = []
+    # 이동한 몬스터 저장
+    newMonsters = dict()
     # 2.몬스터 이동
-    for i, j, d in monsters:
-        liveMonster[i][j] -= 1
-        ni, nj, nd = moveMonster(i, j, d)
-        # 몬스터 이동 위치 추가
-        newMonsters.append((ni, nj, nd))
-        liveMonster[ni][nj] += 1
+    for key in monsters:
+        while monsters[key]:
+            d = monsters[key].pop()
+            newKey, nd = moveMonster(key[0], key[1], d)
+            # 몬스터 이동 위치 추가
+            if newKey in newMonsters:
+                newMonsters[newKey].append(nd)
+            else:
+                newMonsters[newKey] =[nd]
 
     # 3.팩맨 이동
     pi, pj, moveRoute = movePacman(pi, pj)
     # 몬스터 먹기, 시체 생성
-    for i, j in moveRoute:
-        # 몬스터 먹힘
-        liveMonster[i][j] = 0
-        # 시체 생성
-        dieMonsters[i][j] = 2
-        
-    # 살아있는 몬스터 저장
-    monsters = []
-    for i, j, d in newMonsters:
-        if (i, j) not in moveRoute:
-            monsters.append((i, j, d))     
+    for key in moveRoute:
+        if newMonsters.get(key):
+            # 몬스터 먹힘
+            del newMonsters[key]
+            # 시체 생성
+            dieMonsters[key] = 2
 
     # 4.몬스터 시체 소멸
-    for i in range(4):
-        for j in range(4):
-            if dieMonsters[i][j] > 0:
-                dieMonsters[i][j] -= 1
-
+    delM = []
+    for key, value in dieMonsters.items():
+        if value == 1:
+            delM.append(key)
+            continue
+        dieMonsters[key] -= 1
+    # 완전 소멸
+    for key in delM:
+        del dieMonsters[key]
+        
+    monsters = newMonsters
     # 5.몬스터 복제 완성 (알 부화)            
-    for i, j, d in copyMonsters:
-        liveMonster[i][j] += 1
+    for key, value in copyMonsters.items():
         # 부화한 몬스터 위치 추가
-        monsters.append((i, j, d))
+        if key in monsters:
+            monsters[key].extend(value)
+        else:
+            monsters[key] = value       
 
 # 살아남은 몬스터 개수
 ans = 0
-for i in range(4):
-    ans += sum(liveMonster[i])
+for key in monsters:
+    ans += len(monsters[key])
 print(ans)
