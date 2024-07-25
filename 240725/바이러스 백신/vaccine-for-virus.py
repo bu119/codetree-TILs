de0087df0406dfeb4d841dc12b938ab2f8a24199
bu119 <109335452+bu119@users.noreply.github.com>
2024-07-25 11:44:29 +0900

    Title: 바이러스 백신, Time: 324ms, Memory: 29MB, Status: Passed - CodeTree

--- 240725/바이러스 백신/vaccine-for-virus.py
+++ 240725/바이러스 백신/vaccine-for-virus.py
@@ -14,51 +14,49 @@ def bfs(startingPoint):
         queue.append((x, y))
     # 방문한 바이러스 개수 저장
     virusCnt = 0
-    # 현재 탐색에서 걸리는 바이러스 제거 시간 저장
-    time = 0
+    # 시작점에서 바이러스까지 거리 저장
+    dist = 0
 
     while queue:
         x, y = queue.popleft()
 
-        # 바이러스 만났을 때 제거 시간 갱신
-        if board[x][y] == 0 and time < visited[x][y]:
-            time = visited[x][y]
-            # 전체 경우의 최소 시간을 넘으면 바로 탐색 종료
-            if time >= minV:
-                return time
+        # 바이러스 만나면 개수 증가, 거리 갱신
+        if board[x][y] == 0:
+            virusCnt += 1
+            dist = max(visited[x][y], dist)
+            # 최소 시간을 넘으면 탐색 종료
+            if minV <= dist:
+                return dist
 
         # 4방향 탐색
         for k in range(4):
             nx = x + di[k]
             ny = y + dj[k]
             if 0 <= nx < n and 0<= ny < n and visited[nx][ny] == -1 and board[nx][ny] != 1:
-                # 전체 시간, 바이러스 제거 시간 
                 visited[nx][ny] = visited[x][y] + 1
-                # 바이러스이면 개수 증가
-                if board[nx][ny] == 0:
-                    virusCnt += 1
                 queue.append((nx, ny))
 
     # 모든 바이러스를 제거 못 했으면 최대 값 반환
     if virusCnt != totalVirus:
         return 250
-    return time
+    return dist
 
 n, m = map(int, input().split())
 # 전체 바이러스 개수 저장
 totalVirus = 0
+# 병원 위치 저장
 hospitals = []
 board = []
-# 최소 시간 저장
+# 최소 시간 저장 (최대 숫자를 저장하여 비교)
 minV = 250
 
 for i in range(n):
     row = list(map(int, input().split()))
     for j in range(n):
-        # 바이러스이면 개수 세기
+        # 바이러스면 개수 증가
         if row[j] == 0:
             totalVirus += 1
-        # 병원이면 저장
+        # 병원이면 위치 저장
         elif row[j] == 2:
             hospitals.append((i, j))
     # N*N 배열 만들기
@@ -71,7 +69,7 @@ dj = [1, 0, -1, 0]
 # 2.선택된 m개의 병원 탐색
 for selectedHospitals in combinations(hospitals, m):
     removalTime = bfs(selectedHospitals)
-    # 최소값 갱신
+    # 최소 시간 갱신
     minV = min(minV, removalTime)
 
 # 4.모든 바이러스 없앴는지 확인 -> 불가면 -1 반환
