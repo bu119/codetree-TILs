c9c28b40bfdc3e6ccb9205a268081e3cdd161166
bu119 <109335452+bu119@users.noreply.github.com>
2024-10-03 21:16:04 +0900

    Title: 코드트리 투어, Time: 1550ms, Memory: 31MB, Status: Passed - CodeTree

--- 241003/코드트리 투어/codetree-tour.py
+++ 241003/코드트리 투어/codetree-tour.py
@@ -1,37 +1,6 @@
-# 가중치 -> 다익스트라, 신장트리
-# 도시의 수 n 과 간선의 수 m, 그리고 m 개의 간선
-# 간선 정보 (vi, ui, wi): 도시 vi와 도시 ui는 가중치가 wi인 간선으로 연결
-
-# <여행 상품 생성>
-# (id, revenue_id, dest_id) 여행 상품을 추가 -> 관리 목록에 추가
-# 고유한 식별자 id, 얻게되는 매출은 revenue_id, 이 상품의 도착지는 dest_id
-
-# <여행 상품 취소>
-# 고유 식별자 id 에 해당하는 여행 상품이 존재하는 경우, 해당 id 의 여행 상품을 관리 목록에서 삭제
-
-# <최적의 여행 상품 판매>
-# 관리 목록에서 조건에 맞는 최적의 여행 상품을 선택하여 판매
-
-# 선택 조건
-# 1. 여행사의 상품 판매 이득 [revenue_id - cost_id]가 최대인 상품을 우선적으로 고려
-# 2. 같은 값을 가지는 상품이 여러 개 있을 경우 id 가 가장 작은 상품을 선택
-# cost_id 는 현재 여행 상품의 출발지로부터 id 상품의 도착지 dest_id 까지 도달하기 위한 최단거리
-
-# 판매 불가능 상품
-# 1. 출발지로부터 dest_id 에 도달하는 것이 불가능
-# 2. revenue_id < cost_id
-
-# 판매 가능 상품
-# 1. 우선순위가 높은 상품을 1개를 판매, 이 상품의 id 를 출력한 뒤 이 상품을 관리 목록에서 제거
-# 2. 판매 가능한 상품이 전혀 없다면 −1 을 출력하고 상품을 제거하지 않게 됨
-
-# <여행 상품의 출발지 변경>
-# 여행 상품의 출발지를 전부 s 로 변경하는 명령
-# 출발지가 변경됨에 따라 각 상품의 cost_id가 변경될 수 있음
-
 import heapq
 
-# 출발지에서 각 점까지의 최단거리
+# 현재 여행 상품의 출발지로부터 각 도시의 최단거리
 def dijkstra(start):
     # 모든 정점사이의 거리를 최댓값으로 초기화
     # 최단 거리 저장
@@ -63,11 +32,11 @@ def dijkstra(start):
 def add_product_benefits(productId):
     revenue, dest = travelProducts[productId] 
     cost = costs[dest]
-    # 판매 불가 상품
+    # 판매 불가 상품아 아닐 때, 이득 계산
     if revenue >= cost:
         heapq.heappush(productBenefits, (-(revenue - cost), productId))
 
-
+# 전체 상품의 이득 생성
 def find_product_benefits():
     benefits = []
     for productId in travelProducts:
@@ -76,19 +45,23 @@ def find_product_benefits():
         # 판매 불가 상품
         if revenue < cost:
             continue
+        # 판매 이득 [revenue - cost]가 최대인 상품을 우선함
+        # 같은 값을 가지는 상품이 여러 개 있을 경우, id 가 가장 작은 상품
         heapq.heappush(benefits, (-(revenue - cost), productId))
     
     return benefits
 
-
+# 최적의 여행 상품 찾기
 def find_best_product():
     # 이득도 남아있고 여행 상품도 남아 있을 때, 최적 상품 반환
     while productBenefits and travelProducts:
         _, productId = heapq.heappop(productBenefits)
+        # 판매 가능한 상품
         if productId in travelProducts:
+            # 관리 목록에서 제거
             del travelProducts[productId]
             return productId
-    
+    # 판매 가능한 상품이 전혀 없다면 −1 반환
     return -1
 
 
@@ -103,11 +76,13 @@ for _ in range(q):
     
     if type == 100:
         # 코드트리 랜드 건설
+        # 도시의 수 n, 간선의 수 m
         n = info[0]
         m = info[1]
         graph = [[] for _ in range(n+1)]
-        
+
         for i in range(2, m*3, 3):
+            # 도시 v와 도시 u는 가중치가 w인 간선으로 연결
             v, u, w = info[i], info[i+1], info[i+2]
             graph[v].append((u, w))
             graph[u].append((v, w))
@@ -117,6 +92,7 @@ for _ in range(q):
         
     elif type == 200:
         # 여행 상품 생성
+        # 고유한 식별자 id, 매출 revenue, 도착지 dest
         id, revenue, dest = info
         travelProducts[id] = (revenue, dest)
         # 상품 생성시 이득 계산
@@ -125,6 +101,7 @@ for _ in range(q):
     elif type == 300:
         # 여행 상품 취소
         id = info[0]
+        # 해당하는 여행 상품이 존재하는 경우, 여행 상품을 관리 목록에서 삭제
         if id in travelProducts:
             del travelProducts[id]
     
@@ -137,7 +114,7 @@ for _ in range(q):
     else:
         # 여행 상품의 출발지 변경
         s = info[0]
-        # 각 비용 새로 계산
+        # 출발지가 변경됨에 따라 각 상품의 cost_id가 변경
         costs = dijkstra(s)
         # 각 상품 이득 새로 계산
         productBenefits = find_product_benefits()
